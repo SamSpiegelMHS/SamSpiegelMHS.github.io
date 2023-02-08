ac2e8c182f712275d281e524866ed6514fc2d1dd
SamSpiegelMHS <spamseagull1@gmail.com>
2023-02-08 17:58:43 +0000

    I think Im done with everything I need

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -29,21 +29,62 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-createPlatfrom(100,500,700,10);
 
-    
-    
-    
-    
-    
+    //Normal Platforms
+    createPlatform(500, 600, 50, 20);
+    createPlatform(500, 400, 50, 20);
+    createPlatform(650, 350, 15, 20);
+    createPlatform(875, 325, 5,5);
+    createPlatform(1075, 250, 3,3);
+
+
+//Secret Stairs
+    createPlatform(335, 520, 1, 1)
+    createPlatform(330, 515, 1, 1)
+    createPlatform(325, 510, 1, 1)
+    createPlatform(320, 505, 1, 1)
+    createPlatform(315, 500, 1, 1)
+    createPlatform(310, 495, 1, 1)
+    createPlatform(305, 490, 1, 1)
+    createPlatform(300, 485, 1, 1)
+    createPlatform(295, 480, 1, 1)
+    createPlatform(290, 475, 1, 1)
+    createPlatform(285, 470, 1, 1)
+
+
+
+// Cage Left Wall
+    createPlatform(750, 700, 1, 1)
+    createPlatform(750, 675, 1, 1)
+    createPlatform(750, 650, 1, 1)
+    createPlatform(750, 625, 1, 1)
+    createPlatform(750, 600, 1, 1)
+    createPlatform(750, 575, 1, 1)
+    createPlatform(750, 550, 1, 1)
+
+
+
+
+// Cage Right Wall
+    createPlatform(1100, 700, 1, 1)
+    createPlatform(1100, 675, 1, 1)
+    createPlatform(1100, 650, 1, 1)
+    createPlatform(1100, 625, 1, 1)
+    createPlatform(1100, 600, 1, 1)
+    createPlatform(1100, 575, 1, 1)
+    createPlatform(1100, 550, 1, 1)
+
+
+
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
     
-    
-    
+    createCollectable("steve", 1250, 700, .025)
+    createCollectable("grace", 900, 650, 0)
+    createCollectable("steve", 310, 440, 0)
     
     
     // TODO 3
@@ -52,9 +93,9 @@ createPlatfrom(100,500,700,10);
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay)
 
-
-
-
+    createCannon("left", 600, 5000);
+    createCannon("top", 360, 3000);
+    createCannon("bottom", 360, 3000);
 
 
     /////////////////////////////////////////////////
